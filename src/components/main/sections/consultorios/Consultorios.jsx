5fbf1794501b335c138f5993cfc0b52fba4025e6
joao-vitorg <rayanneoliveiradutra@gmail.com>
2023-06-21 20:18:53 -0300

    Header e Main

--- src/components/main/sections/consultorios/Consultorios.jsx
+++ src/components/main/sections/consultorios/Consultorios.jsx
@@ -1,24 +1,21 @@
 import TituloSubtitulo from "../../common/TituloSubtitulo.jsx";
 import styles from "./Consultorios.module.css";
-import Carousel from "react-material-ui-carousel";
 
 export default function Consultorios() {
 	return (
 		<section>
 			<TituloSubtitulo sectionName={"consultorios"} />
-			<Carousel indicators={true}>
-				<div className={styles.container}>
-					<div className={styles.wrapper}>
-						<img src={"../../../../consutorios/primeiro/JOS_3217.JPG"} alt={"a"} className={styles.img} />
-						<img src={"../../../../consutorios/primeiro/JOS_3221.JPG"} alt={"a"} className={styles.img} />
-						<img src={"../../../../consutorios/primeiro/JOS_3222.JPG"} alt={"a"} className={styles.img} />
-						<img src={"../../../../consutorios/primeiro/JOS_3227.JPG"} alt={"a"} className={styles.img} />
-					</div>
-					<div className={styles.wrapperMain}>
-						<img src={"../../../../consutorios/primeiro/JOS_3217.JPG"} alt={"a"} className={styles.imgMain} />
-					</div>
+			<div className={styles.container}>
+				<div className={styles.wrapper}>
+					<img src={"../../../../consultorios/primeiro/JOS_3217.JPG"} alt={"a"} className={styles.img} />
+					<img src={"../../../../consultorios/primeiro/JOS_3221.JPG"} alt={"a"} className={styles.img} />
+					<img src={"../../../../consultorios/primeiro/JOS_3222.JPG"} alt={"a"} className={styles.img} />
+					<img src={"../../../../consultorios/primeiro/JOS_3227.JPG"} alt={"a"} className={styles.img} />
 				</div>
-			</Carousel>
+				<div className={styles.wrapperMain}>
+					<img src={"../../../../consultorios/primeiro/JOS_3217.JPG"} alt={"a"} className={styles.imgMain} />
+				</div>
+			</div>
 		</section>
 	);
 }
--- src/components/main/sections/faq/Faq.jsx
+++ src/components/main/sections/faq/Faq.jsx
@@ -1,9 +1,42 @@
 import TituloSubtitulo from "../../common/TituloSubtitulo.jsx";
+import { useEffect, useState } from "react";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Typography } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export default function Faq() {
+	const [faq, setFaq] = useState([]);
+	const [expanded, setExpanded] = useState(-1);
+
+	const handleChange = (panel) => (event, newExpanded) => {
+		setExpanded(newExpanded ? panel : -1);
+	};
+
+	useEffect(() => {
+		fetch("https://wilton-filho.github.io/PFJS-GitHub/React/projeto/json/faq2.json")
+			.then((value) => value.json())
+			.then((value) => {
+				console.log(value.faqs);
+				setFaq(value.faqs);
+			});
+	}, []);
+
 	return (
 		<section>
 			<TituloSubtitulo sectionName={"faqs"} />
+			<Box>
+				{faq.map((value, index) => {
+					return (
+						<Accordion expanded={expanded === index} onChange={handleChange(index)}>
+							<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content">
+								<Typography>{value["pergunta"]}</Typography>
+							</AccordionSummary>
+							<AccordionDetails>
+								<Typography>{value["resposta"]}</Typography>
+							</AccordionDetails>
+						</Accordion>
+					);
+				})}
+			</Box>
 		</section>
 	);
 }
