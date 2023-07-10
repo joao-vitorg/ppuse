74aa6cd87fd06e1c69799ea1d71c39b3fbadf285
JoaoPedroCardoso <joaopedrocar@hotmail.com>
2023-07-10 18:30:31 -0300

    ajustando video

--- src/components/main/sections/conheca/Conheca.jsx
+++ src/components/main/sections/conheca/Conheca.jsx
@@ -12,11 +12,7 @@ export default function Conheca() {
 				</div>
 
 			
-					<video
-						className={styles["video"]}
-						src="https://github.com/joao-vitorg/ppuse/raw/branch-aparicio/src/components/media/ppuse_institucional.mp4"
-						controls
-					/>
+				<video className={styles["video"]} src={"../../../../media/PPUSE.mp4"} controls />
 				</div>
 			
 		</section>
